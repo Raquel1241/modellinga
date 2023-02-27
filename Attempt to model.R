f5788bd7ef7229926fb9bfb92987fdd40cc5c6e6
SofiaSarigiannidi <126081433+SofiaSarigiannidi@users.noreply.github.com>
2023-02-27 16:40:45 +0100

    Update Attempt to model.R

--- Attempt to model.R
+++ Attempt to model.R
@@ -26,7 +26,7 @@ plotters=data_frame(y, traindat$NO.1, traindat$Cycle)
 ggplot(data = plotters, aes(x = traindat$Cycle, y = traindat$NO.1)) +
   xlab("t (cycles)") +
   ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
-  ggtitle("Figure 1: Battery 1 capacity degradation data (training") +
+  ggtitle("Figure 1: Battery 1 capacity degradation data (training)") +
   geom_line() +
   geom_hline(yintercept=0.88, 
              linetype="dashed", 
@@ -44,7 +44,7 @@ ggplot(data = plotters, aes(x = traindat$Cycle, y = traindat$NO.1)) +
 ggplot(data = plotters, aes(x = traindat$Cycle, y = y)) +
   xlab("t (cycles)") +
   ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
-  ggtitle("Figure 2: Model of Battery 1 capacity degradation data (training") +
+  ggtitle("Figure 2: Model of Battery 1 capacity degradation data (training)") +
   geom_line() +
   geom_hline(yintercept=0.88, 
              linetype="dashed", 
@@ -60,7 +60,58 @@ ggplot(data = plotters, aes(x = traindat$Cycle, y = y)) +
 
 
 
-#Modelling All Batteries
+#Modelling Battery 2
+
+N2=traindat$NO.2
+N2_new=na.omit(N2)
+C2=traindat$Cycle
+C2_new=C2[1:length(N2_new)]
+traindat_N2=data_frame(C2_new,N2_new)
+
+secord=(C2_new)**2
+thirord=(C2_new)**3
+forord=(C2_new)**4
+
+lmres=lm(N2_new~C2_new+secord+thirord+forord, data=traindat_N2)
+y=coef(lmres)[1]+coef(lmres)[2]*C2_new+coef(lmres)[3]*secord+coef(lmres)[4]*thirord+coef(lmres)[5]*forord
+plotters2=data_frame(y, N2_new, C2_new)
+
+ggplot(data = plotters2, aes(x = C2_new, y = y)) +
+  xlab("t (cycles)") +
+  ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
+  ggtitle("Figure 2: Model of Battery 2 capacity degradation data (training)") +
+  geom_line() +
+  geom_hline(yintercept=0.88, 
+             linetype="dashed", 
+             color = "red", 
+             linewidth=0.5) +
+  annotate("text", x=1900, y=0.87, label= "Threshold: 0.88", colour = "red", size = 3) +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 12), 
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "Black"))
+
+ggplot(data = plotters, aes(x = traindat$Cycle, y = traindat$NO.2)) +
+  xlab("t (cycles)") +
+  ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
+  ggtitle("Figure 1: Battery 2 capacity degradation data (training)") +
+  geom_line() +
+  geom_hline(yintercept=0.88, 
+             linetype="dashed", 
+             color = "red", 
+             linewidth=0.5) +
+  annotate("text", x=1900, y=0.87, label= "Threshold: 0.88", colour = "red", size = 3) +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 12), 
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "Black"))
+
+
+#Modelling All Batteries (first try)
 
 secord=(traindat_new$Cycle)**2
 thirord=(traindat_new$Cycle)**3
@@ -88,10 +139,46 @@ ggplot(data = plotters, aes(x = traindat_new$Cycle, y = y)) +
         axis.line = element_line(colour = "Black"))
 
 
+#Modelling All Batteries (second try)
+
+bigvector= c()
+bigveccyc= c()
+for (i in 2:length(traindat)){
+  toadd=na.omit(traindat[1:length(traindat$Cycle),i])
+  bigvector=append(bigvector, toadd)
+  bigveccyc=append(bigveccyc, traindat$Cycle[1:length(toadd)])
+}
+
+bigtraindata=data_frame(bigveccyc,bigvector)
+
+firord=(bigtraindata$bigveccyc)
+secord=(bigtraindata$bigveccyc)**2
+thirord=(bigtraindata$bigveccyc)**3
+forord=(bigtraindata$bigveccyc)**4
+
+lmres=lm(bigtraindata$bigvector~firord+secord+thirord+forord, data=bigtraindata)
+y=coef(lmres)[1]+coef(lmres)[2]*firord+coef(lmres)[3]*secord+coef(lmres)[4]*thirord+coef(lmres)[5]*forord
+plotters=data_frame(y, bigtraindata$bigvector, bigtraindata$bigveccyc)
+
+ggplot(data = plotters, aes(x = bigtraindata$bigveccyc, y = y)) +
+  xlab("t (cycles)") +
+  ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
+  ggtitle("Figure 4: Second Model of battery capacity degradation data (training)") +
+  geom_line() +
+  geom_hline(yintercept=0.88, 
+             linetype="dashed", 
+             color = "red", 
+             linewidth=0.5) +
+  annotate("text", x=1900, y=0.87, label= "Threshold: 0.88", colour = "red", size = 3) +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 12), 
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "Black"))
 
 
 
-  
 
 
 
