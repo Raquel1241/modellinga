502f8772d271c2f0bed6e4b9f1dbc2c5e89ce46e
SofiaSarigiannidi <126081433+SofiaSarigiannidi@users.noreply.github.com>
2023-02-27 14:07:58 +0100

    update

--- Attempt to model.R
+++ Attempt to model.R
@@ -12,23 +12,6 @@ traindat=read.csv("C:/Users/sofia/OneDrive/Random/GitHub/modellinga/Data/Battery
 #traindat = read.csv("/Users/tyrenkoning/Documents/GitHub/modellinga/Battery_train.csv")
 traindat_new = gather(traindat, variable, value, -Cycle)
 
-ggplot(data = traindat_new, aes(x = Cycle, y = value, group = variable)) +
-  xlab("t (cycles)") +
-  ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
-  ggtitle("Figure 1: Battery capacity degradation data (training") +
-  geom_line() +
-  geom_hline(yintercept=0.88, 
-             linetype="dashed", 
-             color = "red", 
-             linewidth=0.5) +
-  annotate("text", x=1900, y=0.87, label= "Threshold: 0.88", colour = "red", size = 3) +
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5, size = 12), 
-        panel.border = element_blank(), 
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), 
-        axis.line = element_line(colour = "Black"))
-
 
 #Modelling Battery 1
 
@@ -43,7 +26,25 @@ plotters=data_frame(y, traindat$NO.1, traindat$Cycle)
 ggplot(data = plotters, aes(x = traindat$Cycle, y = traindat$NO.1)) +
   xlab("t (cycles)") +
   ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
-  ggtitle("Figure 1: Battery capacity degradation data (training") +
+  ggtitle("Figure 1: Battery 1 capacity degradation data (training") +
+  geom_line() +
+  geom_hline(yintercept=0.88, 
+             linetype="dashed", 
+             color = "red", 
+             linewidth=0.5) +
+  annotate("text", x=1900, y=0.87, label= "Threshold: 0.88", colour = "red", size = 3) +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 12), 
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "Black"))
+
+
+ggplot(data = plotters, aes(x = traindat$Cycle, y = y)) +
+  xlab("t (cycles)") +
+  ylab(bquote("Capacity" ~ C["t,i"] ~ "(Ah)")) +
+  ggtitle("Figure 2: Model of Battery 1 capacity degradation data (training") +
   geom_line() +
   geom_hline(yintercept=0.88, 
              linetype="dashed", 
@@ -85,7 +86,11 @@ ggplot(data = plotters, aes(x = traindat_new$Cycle, y = y)) +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), 
         axis.line = element_line(colour = "Black"))
-  
+
+
+
+
+
   
 
 
